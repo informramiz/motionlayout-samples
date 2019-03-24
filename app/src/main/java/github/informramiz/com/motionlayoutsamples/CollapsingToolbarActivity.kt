791640fc65bfc257ab14be97a4cb5e4616439f46
Ramiz Raja <informramiz@gmail.com>
2019-03-24 22:15:04 +0500

    feat: Write native collapsing toolbar

--- app/src/main/java/github/informramiz/com/motionlayoutsamples/CollapsingToolbarActivity.kt
+++ app/src/main/java/github/informramiz/com/motionlayoutsamples/CollapsingToolbarActivity.kt
@@ -1,12 +1,13 @@
 package github.informramiz.com.motionlayoutsamples
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class CollapsingToolbarActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_collapsible_toolbar)
+//        setSupportActionBar(toolbar)
     }
 }
