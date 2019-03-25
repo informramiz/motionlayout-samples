cbcd4ac6807d37d63a587ba85f4bf5d49e3d063b
Ramiz Raja <informramiz@gmail.com>
2019-03-25 22:17:10 +0500

    chore: Add blank activity

---
+++ app/src/main/java/github/informramiz/com/motionlayoutsamples/ViewPagerActivity.kt
@@ -0,0 +1,12 @@
+package github.informramiz.com.motionlayoutsamples
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ViewPagerActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_pager)
+    }
+}
